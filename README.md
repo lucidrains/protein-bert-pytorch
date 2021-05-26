390e0b02d488892cbb08485c819f8f89ad5ac077
Phil Wang <lucidrains@gmail.com>
2021-05-26 11:29:27 -0700

    scaffold

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ Implementation of <a href="https://www.biorxiv.org/content/10.1101/2021.05.24.44
 @article {Brandes2021.05.24.445464,
 	author 		= {Brandes, Nadav and Ofer, Dan and Peleg, Yam and Rappoport, Nadav and Linial, Michal},
 	title 		= {ProteinBERT: A universal deep-learning model of protein sequence and function},
-	elocation-id = {2021.05.24.445464},
 	year 		= {2021},
 	doi 		= {10.1101/2021.05.24.445464},
 	publisher 	= {Cold Spring Harbor Laboratory},
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="protein-bert-pytorch",
+    packages=find_packages(),
+    version="0.0.1",
+    license="MIT",
+    description="ProteinBERT - Pytorch",
+    author="Phil Wang",
+    author_email="lucidrains@gmail.com",
+    url="https://github.com/lucidrains/protein-bert-pytorch",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "attention mechanism",
+        "protein sequences"
+    ],
+    install_requires=[
+        "einops>=0.3",
+        "torch>=1.6",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
